f71471948705992a8161f08b4dbdd75b0599571a
Titouan DENYS <titouan.denys@gmail.com>
2021-05-04 12:02:02 +0200

    Ajout méthode "getUtilisateurByPseudoOrEmail" dans DAL

--- src/main/java/fr/eni/projetenchere/dal/UtilisateurDAOImpl.java
+++ src/main/java/fr/eni/projetenchere/dal/UtilisateurDAOImpl.java
@@ -11,9 +11,30 @@ import fr.eni.projetenchere.bo.Utilisateur;
 
 public class UtilisateurDAOImpl implements UtilisateurDAO {
 	
+	private String GET_UTILISATEUR_BY_PSEUDO_OR_EMAIL = "SELECT * FROM UTILISATEURS WHERE pseudo = ? OR email = ?";
 	private String GET_UTILISATEUR_BY_PSEUDO = "SELECT * FROM UTILISATEURS WHERE pseudo = ?";
 	private String GET_UTILISATEUR_BY_EMAIL = "SELECT * FROM UTILISATEURS WHERE email = ?";
 	private String INSERT_UTILISATEUR = "INSERT INTO UTILISATEURS(pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+	
+	@Override
+	public Utilisateur getUtilisateurByPseudoOrEmail(String pseudo) throws Exception {
+		
+		Utilisateur u = null;
+		try(Connection cnx = ConnectionProvider.getConnection()){
+			PreparedStatement stmt = cnx.prepareStatement(GET_UTILISATEUR_BY_PSEUDO_OR_EMAIL);
+			stmt.setString(1, pseudo);
+			ResultSet rs = stmt.executeQuery();
+			if(rs.next()) {
+				u = new Utilisateur(rs.getString("pseudo"), rs.getString("nom"), rs.getString("prenom"), rs.getString("email"), rs.getString("telephone"),
+						rs.getString("rue"),rs.getString("code_postal"),rs.getString("ville"),rs.getString("mot_de_passe"), rs.getInt("credit"), rs.getBoolean("administrateur"));
+			}
+		}
+		catch(Exception e) {
+			throw new Exception(GET_UTILISATEUR_BY_PSEUDO_OR_EMAIL);
+		}
+		
+		return u;
+	}
 	
 	@Override
 	public Utilisateur getUtilisateurByPseudo(String pseudo) throws Exception {
@@ -75,7 +96,6 @@ public class UtilisateurDAOImpl implements UtilisateurDAO {
 		catch(Exception e) {
 			throw new Exception(INSERT_UTILISATEUR);
 		}
-
 	}
 	
 	/*public List<Utilisateur> getAll() throws Exception {
