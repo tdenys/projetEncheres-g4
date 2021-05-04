85555aa981051446a7a3061d9c1e6bf4c3ea9417
Diego Bonnamy <diegobonnamy2001@gmail.com>
2021-05-04 10:06:52 +0200

    Couche IHM pour seConnecter

---
+++ src/main/java/fr/eni/projetenchere/ihm/Connexion.java
@@ -0,0 +1,65 @@
+package fr.eni.projetenchere.ihm;
+
+import java.io.IOException;
+import java.time.format.DateTimeParseException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.projetenchere.bll.UtilisateurManager;
+import fr.eni.projetenchere.bll.UtilisateurManagerFact;
+import fr.eni.projetenchere.bo.Utilisateur;
+
+/**
+ * Servlet implementation class Connexion
+ */
+@WebServlet("/Connexion")
+public class Connexion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private UtilisateurManager manager = UtilisateurManagerFact.getInstance();
+       
+    public Connexion() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/index.jsp");
+		rd.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		// Création des paramètres à récupérer
+		String user = null;
+		String password = null;
+		
+		// Lecture des paramètres
+		user = request.getParameter("user");
+		password = request.getParameter("password");
+		
+		// Vérification de l'existance en base
+		try {
+			Utilisateur theUser = manager.getUtilisateurByPseudoAndMDP(user, password);
+			System.out.println("ok");
+			//RequestDispatcher rd = request.getRequestDispatcher("/");
+			//rd.forward(request, response);
+		} catch (Exception e) {
+			request.setAttribute("erreur", "Mot de passe incorrect");
+			// Transfert de l'affichage à la JSP
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/index.jsp");
+			rd.forward(request, response);
+			
+			System.out.println("ko");
+			e.printStackTrace();
+		}
+		
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,54 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+	<jsp:include page="/WEB-INF/fragments/head.jsp"/>
+	
+	<body style="margin: 25px;">
+	
+		<div class="row text-left">
+			<div class="col">
+				<h1>ENI - Enchères</h1>
+			</div>
+		</div>
+	
+		<div class="row text-center">
+			<div class="col">
+				<!-- Formulaire de connexion -->
+				<form method="post" action="Connexion">
+					<!-- USER -->
+					<label for="user">Identifiant</label>
+					<input type="text" class="form-control" id="user" name="user">
+					
+					<!-- PASSWORD -->
+					<label for="password">Mot de passe</label>
+					<input type="password" class="form-control" id="password" name="password">
+					
+					<!-- SUBMIT -->
+					<input type="submit" class="btn btn-dark text-light" value="Connexion"/>	
+				</form>
+				
+				<!-- Affichage message d'erreur -->
+				<%
+					if(request.getParameter("erreur")!=null)
+					{
+				%>
+				<div class="alert alert-danger" role="alert">
+				  <% System.out.println(request.getParameter("erreur")); %>
+				</div>
+				<%
+					}
+				%>
+				
+			</div>
+		</div>
+		
+		<div class="row text-center">
+			<div class="col">
+				<!-- Bouton création de compte -->
+				<a href="${pageContext.request.contextPath}/signin" class="btn btn-lg btn-dark text-light">Créer un compte</a>
+			</div>
+		</div>
+		
+	</body>
+</html>
