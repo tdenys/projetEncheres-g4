3f38c6bc49947f9b6c878d6285ded6ef5b6ccbfc
Titouan DENYS <titouan.denys@gmail.com>
2021-05-06 14:35:30 +0200

    GetAll() et insertArticle() dans ArticleManagerImpl

--- src/main/java/fr/eni/projetenchere/bll/article/ArticleManager.java
+++ src/main/java/fr/eni/projetenchere/bll/article/ArticleManager.java
@@ -1,11 +1,14 @@
 package fr.eni.projetenchere.bll.article;
 
 import java.util.Date;
+import java.util.List;
 
 import fr.eni.projetenchere.bo.Article;
 import fr.eni.projetenchere.bo.Categorie;
 
 public interface ArticleManager {
+	
+	public List<Article> getAll() throws Exception;
 	
 	public Article insertArticle(String pseudoVendeur, String nomArticle, String description, Date dateDebutEnchere, Date dateFinEnchere, int prixInitial, int numCategorie) throws Exception;
 }
--- src/main/java/fr/eni/projetenchere/bll/article/ArticleManagerImpl.java
+++ src/main/java/fr/eni/projetenchere/bll/article/ArticleManagerImpl.java
@@ -1,20 +1,33 @@
 package fr.eni.projetenchere.bll.article;
 
 import java.util.Date;
+import java.util.List;
 
 import fr.eni.projetenchere.bo.Article;
+import fr.eni.projetenchere.dal.ArticleDAO;
+import fr.eni.projetenchere.dal.ArticleDAOFactory;
 import fr.eni.projetenchere.dal.UtilisateurDAO;
 import fr.eni.projetenchere.dal.UtilisateurDAOFactory;
 
 public class ArticleManagerImpl implements ArticleManager {
 	
-	//private ArticleDAO articleDAO = ArticleDAOFactory.getArticleDAO();
+	private ArticleDAO articleDAO = ArticleDAOFactory.getArticleDAO();
 
 	private UtilisateurDAO utilisateurDAO = UtilisateurDAOFactory.getUtilisateurDAO(); 
 	
 	@Override
+	public List<Article> getAll() throws Exception {
+
+		return articleDAO.getAll();
+	}
+	
+	@Override
 	public Article insertArticle(String pseudoVendeur, String nomArticle, String description, Date dateDebutEnchere,
 			Date dateFinEnchere, int prixInitial, int numCategorie) throws Exception {
+		
+		//Validation des saisies utilisateur
+		datesValides(dateDebutEnchere, dateFinEnchere);
+		stringValide(description, "Description");
 		
 		//on récupère l'id du vendeur de l'article
 		int numVendeur = utilisateurDAO.getUtilisateurByPseudo(pseudoVendeur).getNo_utilisateurs();
@@ -26,5 +39,17 @@ public class ArticleManagerImpl implements ArticleManager {
 		return articleDAO.insertArticle(articleAInserer); 
 	}
 	
+	private void datesValides(Date dateDebut, Date dateFin) throws Exception {
+		if(dateFin.after(dateDebut)) {
+			throw new Exception("La date de début ne peut pas être supérieur à la date de fin.");
+		}
+		
+	}
+	
+	private void stringValide(String str, String type) throws Exception {
+        if(str.contains("<")) {
+        	throw new Exception(type + " incorrect"); 
+		}
+	}
 	
 }
