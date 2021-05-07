cb2ec192919816012ad8ac04bff002451492dd91
Alexandre GUILLOTEAU <alexandreguilloteau99@gmail.com>
2021-05-07 14:08:06 +0200

    connection provider public

--- src/main/java/fr/eni/projetenchere/dal/ConnectionProvider.java
+++ src/main/java/fr/eni/projetenchere/dal/ConnectionProvider.java
@@ -8,11 +8,11 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
-abstract class ConnectionProvider {
+public abstract class ConnectionProvider {
 	private static DataSource dataSource;
 	
 	/**
-	 * Au chargement de la classe, la DataSource est recherchée dans l'arbre JNDI
+	 * Au chargement de la classe, la DataSource est recherchï¿½e dans l'arbre JNDI
 	 */
 	static
 	{
@@ -22,13 +22,13 @@ abstract class ConnectionProvider {
 			ConnectionProvider.dataSource = (DataSource)context.lookup("java:comp/env/jdbc/pool_cnx");
 		} catch (NamingException e) {
 			e.printStackTrace();
-			throw new RuntimeException("Impossible d'accéder à la base de données");
+			throw new RuntimeException("Impossible d'accï¿½der ï¿½ la base de donnï¿½es");
 		}
 	}
 	
 	/**
-	 * Cette méthode retourne une connection opérationnelle issue du pool de connexion
-	 * vers la base de données. 
+	 * Cette mï¿½thode retourne une connection opï¿½rationnelle issue du pool de connexion
+	 * vers la base de donnï¿½es. 
 	 * @return
 	 * @throws SQLException
 	 */
