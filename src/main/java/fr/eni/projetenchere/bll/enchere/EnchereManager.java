ffcf855d2b769976dbc47ba88ac4e6691ba51f42
Alexandre GUILLOTEAU <alexandreguilloteau99@gmail.com>
2021-05-07 14:40:43 +0200

    ajout fonction insertEnchere

--- src/main/java/fr/eni/projetenchere/bll/enchere/EnchereManager.java
+++ src/main/java/fr/eni/projetenchere/bll/enchere/EnchereManager.java
@@ -10,7 +10,7 @@ public interface EnchereManager {
 	
 	public Enchere insertEnchere(int montant_enchere, Article article, Utilisateur utilisateur) throws Exception;
 
-	public Enchere getEnchereByIdArticle(int numArticle) throws Exception;
+	/*public Enchere getEnchereByIdArticle(int numArticle) throws Exception;
 
-	void removeEnchere(Enchere e) throws Exception;
+	void removeEnchere(Enchere e) throws Exception;*/
 }
--- src/main/java/fr/eni/projetenchere/bll/enchere/EnchereManagerImpl.java
+++ src/main/java/fr/eni/projetenchere/bll/enchere/EnchereManagerImpl.java
@@ -12,8 +12,9 @@ import fr.eni.projetenchere.dal.article.ArticleDAO;
 import fr.eni.projetenchere.dal.article.ArticleDAOFactory;
 import fr.eni.projetenchere.dal.ConnectionProvider;
 import fr.eni.projetenchere.dal.enchere.EnchereDAO;
+import fr.eni.projetenchere.dal.enchere.EnchereDAOFactory;
 import fr.eni.projetenchere.dal.utilisateur.UtilisateurDAO;
-import fr.eni.projetenchere.dal.utilisateur.UtilisateurDAO;
+import fr.eni.projetenchere.dal.utilisateur.UtilisateurDAOFactory;
 
 public class EnchereManagerImpl implements EnchereManager {
 	
@@ -30,35 +31,36 @@ public class EnchereManagerImpl implements EnchereManager {
 		if(article.getPrix_vente() > montant_enchere && (credit_actuel_nouvel_utilisateur - montant_enchere) >= 0) {
 			try {
 				// l'utilisateur qui avait encherie recupère ses credit
-				Utilisateur ancien_utilisateur = new Utilisateur();
+				Utilisateur ancien_utilisateur = enchereDAO.getUtilisateurLastEnchere(article.getNo_article());
 				int credit_actuel_ancien_utilisateur = article.getUtilisateur().getCredit();
 				article.getUtilisateur().setCredit(credit_actuel_ancien_utilisateur + article.getPrix_vente());
 				
 				// l'utilisateur qui a encherie perd ses credit
 				nouvel_utilisateur.setCredit(credit_actuel_nouvel_utilisateur - montant_enchere);
 				
-				// la connexion de commit pas auto
+				// la connexion ne doit pas commit auto
 				cnx.setAutoCommit(false);
 				
 				// maj du nouvel utilisateur avec les credit en moins
-				utilisateurDAO.updateUtilisateur(cnx, nouvel_utilisateur);
+				utilisateurDAO.updateUtilisateur(cnx, nouvel_utilisateur, nouvel_utilisateur.getNom());
 				
 				// maj de l'article dans la bdd
 				article.setPrix_vente(montant_enchere);
 				articleDAO.updateArticle(cnx, article);
 				
 				// insert de l'enchere
-				en = new Enchere(new Date(), montant_enchere, article, nouvel_utilisateur)
+				en = new Enchere(new Date(), montant_enchere, article, nouvel_utilisateur);
 				enchereDAO.insertEnchere(cnx, en);
 				cnx.commit();
 			} catch (Exception e) {
-				e.printStackTrace();
+				cnx.rollback();
 			}
 		}
-		return e;
+		cnx.close();
+		return en;
 	}
 	
-	@Override
+	/*@Override
 	public void removeEnchere(Article enchere) throws Exception {
 		articleDAO.removeArticle(article);
 	}
@@ -73,6 +75,6 @@ public class EnchereManagerImpl implements EnchereManager {
 	public void removeEnchere(Enchere e) throws Exception {
 		// TODO Auto-generated method stub
 		
-	}
+	}*/
 	
 }
